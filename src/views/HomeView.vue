01e775cc57a44cd16d8c7bb79015f7283e8e5669
David <david@jarkowski.name>
2025-11-09 23:54:49 +0100

    cleaned

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+<main>
+  <div class="home"></div>
+</main>
 </template>
