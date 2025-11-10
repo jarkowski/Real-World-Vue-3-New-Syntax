0d9dd44c95661fb79b20f5895e5eb8b63d6b42b2
David <david@jarkowski.name>
2025-11-10 01:17:04 +0100

    ok

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,17 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div id="layout">
-    <header>
-      <div class="wrapper">
-        <nav>
-          <RouterLink to="/">Home</RouterLink> |
-          <RouterLink to="/about">About</RouterLink>
-        </nav>
-      </div>
-    </header>
-    <RouterView />
-  </div>
+<div id="layout">
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Events</RouterLink> |
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+</div>
 </template>
 
 <style>
@@ -24,16 +24,20 @@ import { RouterLink, RouterView } from 'vue-router'
   text-align: center;
   color: #2c3e50;
 }
+
 nav {
   padding: 30px;
 }
+
 nav a {
   font-weight: bold;
   color: #2c3e50;
 }
+
 nav a.router-link-exact-active {
   color: #42b983;
 }
+
 h2 {
   font-size: 20px;
 }
