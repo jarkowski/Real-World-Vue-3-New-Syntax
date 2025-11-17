e320cd158d85607ea596d1d8cab5bba8545a3a13
David <david@jarkowski.name>
2025-11-17 22:49:17 +0100

    fix no upate yet

--- src/router/index.js
+++ src/router/index.js
@@ -6,13 +6,11 @@ import EventDetailsView from '../views/EventDetailsView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-
-
     {
       path: '/',
       name: 'event-list',
       component: EventList,
-      props: route => ({ page: parseInt(route.query.page) || 1 })
+      props: (route) => ({ page: parseInt(route.query.page) || 1 }),
     },
 
     {
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -3,35 +3,43 @@ import EventCard from '@/components/EventCard.vue'
 import { onMounted, ref } from 'vue'
 import EventService from '@/services/EventService'
 
-
-const props = defineProps(["page"])
+const props = defineProps(['page'])
 
 const events = ref(null)
 
 const fetchEvents = () => {
-
   EventService.getEvents(2, props.page)
     .then((response) => {
       events.value = response.data
     })
     .catch((error) => {
       console.log(error)
-    });
-
-
-
+    })
 }
 
 onMounted(() => {
   fetchEvents()
-});
+})
 </script>
 
 <template>
-<h1>Events For Good</h1>
-<div class="events">
-  <EventCard v-for="event in events" :key="event.id" :event="event" />
-</div>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <router-link
+      :to="{ name: 'event-list', query: { page: page - 1 } }"
+      rel="prev"
+      v-if="page != 1"
+    >
+      Prev Page</router-link
+    >
+    <router-link
+      :to="{ name: 'event-list', query: { page: page + 1 } }"
+      rel="next"
+    >
+      Next Page
+    </router-link>
+  </div>
 </template>
 
 <style scoped>
